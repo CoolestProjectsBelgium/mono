edf21fa1b49d22c85910d3c98be668d8c99014a9
Erik Woidt <erik@woidt.be>
2025-06-09 12:19:33 +0200

    linter cleanup + make test successfull

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from '../src/app.module'; 
+import { AppModule } from '../src/app.module';
 import { seedDatabase } from '../src/seeder/seed';
 import { Event } from '../src/models/event.model';
 import { TshirtGroup } from '../src/models/tshirt_group.model';
@@ -12,13 +12,12 @@ import { Location } from '../src/models/location.model';
 import { TshirtGroupTranslation } from '../src/models/tshirt_group_translation.model';
 import { EventTable } from '../src/models/event_table.model';
 import { EmailTemplate } from '../src/models/email_template.model';
-import { RegistrationService } from '../src/registration/registration.service';
-import { ParticipantService } from '../src/participant/participant.service';
-import { TokensService } from '../src/tokens/tokens.service';
+//import { RegistrationService } from '../src/registration/registration.service';
+//import { ParticipantService } from '../src/participant/participant.service';
+//import { TokensService } from '../src/tokens/tokens.service';
 import { getModelToken } from '@nestjs/sequelize';
 import { env } from 'process';
-import { MailerService } from '../src/mailer/mailer.service';
-
+//import { MailerService } from '../src/mailer/mailer.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -39,9 +38,11 @@ describe('AppController (e2e)', () => {
       app.get<typeof Event>(getModelToken(Event)),
       app.get<typeof TshirtGroup>(getModelToken(TshirtGroup)),
       app.get<typeof Question>(getModelToken(Question)),
-      app.get<typeof QuestionTranslation>(getModelToken(QuestionTranslation)), 
+      app.get<typeof QuestionTranslation>(getModelToken(QuestionTranslation)),
       app.get<typeof Tshirt>(getModelToken(Tshirt)),
-      app.get<typeof TshirtGroupTranslation>(getModelToken(TshirtGroupTranslation)),
+      app.get<typeof TshirtGroupTranslation>(
+        getModelToken(TshirtGroupTranslation),
+      ),
       app.get<typeof Location>(getModelToken(Location)),
       app.get<typeof EventTable>(getModelToken(EventTable)),
       app.get<typeof EmailTemplate>(getModelToken(EmailTemplate)),
@@ -55,9 +56,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/settings')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        '{"maxAge":18,"minAge":7,"guardianAge":16,"enviroment":"production","waitingListActive":false,"maxUploadSize":2147483647,"startDateEvent":"2024-09-01T00:00:00.000Z","tshirtDate":"2025-04-01T00:00:00.000Z","isActive":true,"eventBeginDate":"2024-09-01T00:00:00.000Z","registrationOpenDate":"2024-11-01T00:00:00.000Z","registrationClosedDate":"2025-04-01T00:00:00.000Z","projectClosedDate":"2025-04-12T00:00:00.000Z","officialStartDate":"2025-04-26T00:00:00.000Z","eventEndDate":"2025-08-31T00:00:00.000Z","eventTitle":"Coolest Projects 2025","isRegistrationOpen":false,"isProjectClosed":true,"maxRegistration":64,"maxParticipants":3}',
+      );
   });
-
 
   afterAll(async () => {
     if (app) {
