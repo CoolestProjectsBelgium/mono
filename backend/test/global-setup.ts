ce08318c5d2837b35953dfa27c20e471ad127c12
Erik Woidt <erik@woidt.be>
2025-06-09 13:26:57 +0200

    add extra translations, phpmyadmin fix validation

--- backend/test/global-setup.ts
+++ backend/test/global-setup.ts
@@ -2,8 +2,6 @@ import { config } from 'dotenv';
 import { join } from 'path';
 
 export default async () => {
-  config({ path: join(__dirname, '..', '.env.test') });
-
   // Set environment variables for testing
   process.env.DB_NAME = process.env.DB_NAME_TEST;
   process.env.DB_HOST = process.env.DB_HOST_TEST;
