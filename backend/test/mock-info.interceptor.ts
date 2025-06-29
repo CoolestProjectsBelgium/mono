bd911e064a6c4967dd20855c7ac47d25e7f64337
Erik Woidt <erik@woidt.be>
2025-06-29 12:47:54 +0200

    add changable parameters to mock

--- backend/test/mock-info.interceptor.ts
+++ backend/test/mock-info.interceptor.ts
@@ -1,14 +1,24 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MockInfoInterceptor implements NestInterceptor {
+  constructor(
+    private readonly eventId: number = 999,
+    private readonly language: string = 'en',
+  ) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
 
     req.info = {
-      currentEvent: 1,
-      language: 'en',
+      currentEvent: this.eventId,
+      language: this.language,
     };
 
     return next.handle();
