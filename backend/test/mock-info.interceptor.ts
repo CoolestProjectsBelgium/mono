7e4d41da3c3a52f8af97b086289ba9e22d385c71
Erik Woidt <erik@woidt.be>
2025-06-29 17:20:32 +0200

    mock mailer during test

--- backend/test/mock-info.interceptor.ts
+++ backend/test/mock-info.interceptor.ts
@@ -9,19 +9,13 @@ import { InfoDto } from '../src/dto/info.dto';
 
 @Injectable()
 export class MockInfoInterceptor implements NestInterceptor {
-  constructor(
-    private info: InfoDto,
-  ) {
-    console.error('MockInfoInterceptor initialized with info:', this.info);
-  }
+  constructor(private info: InfoDto) {}
   
   setInfo(info: InfoDto) {
-    console.log('updated:', this.info);
     this.info = info;
   }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.error('MockInfoInterceptor called', this.info);
     const request = context.switchToHttp().getRequest();
     request['info'] = this.info;
     return next.handle();
