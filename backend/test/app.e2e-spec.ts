9e23a917ea36ba04c2c3c4b00611e133a9f78a8d
DylanGybels <66937076+DylanGybels@users.noreply.github.com>
2025-07-06 16:34:09 +0200

    Moved seeding of database to global setup of tests

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,22 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { seedDatabase } from '../src/seeder/seed';
-import { Event } from '../src/models/event.model';
-import { TshirtGroup } from '../src/models/tshirt_group.model';
-import { Question } from '../src/models/question.model';
-import { QuestionTranslation } from '../src/models/question_translation.model';
-import { Tshirt } from '../src/models/tshirt.model';
-import { Location } from '../src/models/location.model';
-import { TshirtGroupTranslation } from '../src/models/tshirt_group_translation.model';
-import { EventTable } from '../src/models/event_table.model';
-import { EmailTemplate } from '../src/models/email_template.model';
-import { TshirtTranslation } from '../src/models/tshirt_translation.model';
 //import { RegistrationService } from '../src/registration/registration.service';
 //import { ParticipantService } from '../src/participant/participant.service';
 //import { TokensService } from '../src/tokens/tokens.service';
-import { getModelToken } from '@nestjs/sequelize';
-import { Sequelize } from 'sequelize-typescript';
 import { InfoInterceptor } from '../src/info.interceptor';
 import { MockInfoInterceptor } from './mock-info.interceptor'; // Import your mock interceptor
 import * as nodemailer from 'nodemailer';
@@ -144,29 +131,6 @@ describe('AppController (e2e)', () => {
       app = moduleFixture.createNestApplication();
       app.useGlobalInterceptors(mockInterceptor);
       await app.init();
-
-      // Clean test database
-      const sequelize = app.get(Sequelize);
-      await sequelize.sync({ force: true });
-
-      // load testing data
-      await seedDatabase(
-        app.get<typeof Event>(getModelToken(Event)),
-        app.get<typeof TshirtGroup>(getModelToken(TshirtGroup)),
-        app.get<typeof Question>(getModelToken(Question)),
-        app.get<typeof QuestionTranslation>(getModelToken(QuestionTranslation)),
-        app.get<typeof Tshirt>(getModelToken(Tshirt)),
-        app.get<typeof TshirtGroupTranslation>(
-          getModelToken(TshirtGroupTranslation),
-        ),
-        app.get<typeof Location>(getModelToken(Location)),
-        app.get<typeof EventTable>(getModelToken(EventTable)),
-        app.get<typeof EmailTemplate>(getModelToken(EmailTemplate)),
-        app.get<typeof TshirtTranslation>(getModelToken(TshirtTranslation)),
-        //app.get<RegistrationService>(RegistrationService),
-        //app.get<ParticipantService>(ParticipantService),
-        //app.get<TokensService>(TokensService),
-      );
     },
     1 * 60 * 1000,
   );
--- backend/test/global-setup.ts
+++ backend/test/global-setup.ts
@@ -1,4 +1,20 @@
 import 'reflect-metadata';
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { seedDatabase } from '../src/seeder/seed';
+import { Event } from '../src/models/event.model';
+import { TshirtGroup } from '../src/models/tshirt_group.model';
+import { Question } from '../src/models/question.model';
+import { QuestionTranslation } from '../src/models/question_translation.model';
+import { Tshirt } from '../src/models/tshirt.model';
+import { Location } from '../src/models/location.model';
+import { TshirtGroupTranslation } from '../src/models/tshirt_group_translation.model';
+import { EventTable } from '../src/models/event_table.model';
+import { EmailTemplate } from '../src/models/email_template.model';
+import { TshirtTranslation } from '../src/models/tshirt_translation.model';
+import { getModelToken } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
 
 export default async () => {
   // Set environment variables for testing
@@ -7,4 +23,39 @@ export default async () => {
   process.env.DB_PORT = process.env.DB_PORT;
   process.env.DB_USER = process.env.DB_USER_TEST;
   process.env.DB_PASS = process.env.DB_PASS_TEST;
+
+  // Clear database and seed with test data
+  let app: INestApplication;
+
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+    providers: [],
+  })
+    .compile();
+
+  app = moduleFixture.createNestApplication();
+  await app.init();
+  // Clean test database
+  const sequelize = app.get(Sequelize);
+  await sequelize.sync({ force: true });
+
+  // load testing data
+  await seedDatabase(
+    app.get<typeof Event>(getModelToken(Event)),
+    app.get<typeof TshirtGroup>(getModelToken(TshirtGroup)),
+    app.get<typeof Question>(getModelToken(Question)),
+    app.get<typeof QuestionTranslation>(getModelToken(QuestionTranslation)),
+    app.get<typeof Tshirt>(getModelToken(Tshirt)),
+    app.get<typeof TshirtGroupTranslation>(
+      getModelToken(TshirtGroupTranslation),
+    ),
+    app.get<typeof Location>(getModelToken(Location)),
+    app.get<typeof EventTable>(getModelToken(EventTable)),
+    app.get<typeof EmailTemplate>(getModelToken(EmailTemplate)),
+    app.get<typeof TshirtTranslation>(getModelToken(TshirtTranslation)),
+    //app.get<RegistrationService>(RegistrationService),
+    //app.get<ParticipantService>(ParticipantService),
+    //app.get<TokensService>(TokensService),
+  );
+  await app.close();
 };
