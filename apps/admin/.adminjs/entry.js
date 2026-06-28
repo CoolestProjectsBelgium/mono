ac5d6c0ca08937d779987bff98375968ccd9c64c
Erik Woidt <erik@woidt.be>
2026-06-28 06:14:01 +0000

    adminjs

--- apps/admin/.adminjs/entry.js
+++ apps/admin/.adminjs/entry.js
@@ -1,3 +1,5 @@
 AdminJS.UserComponents = {}
 import Login from '../src/components/Login'
 AdminJS.UserComponents.Login = Login
+import Dashboard from '../src/components/Dashboard'
+AdminJS.UserComponents.Dashboard = Dashboard
