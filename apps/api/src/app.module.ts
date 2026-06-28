913dc91ac48c8f815e327640ad17b91d0fa8cc61
Erik Woidt <erik@woidt.be>
2026-06-28 10:50:07 +0000

    fix build script & api boot

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -66,7 +66,7 @@ import { AuthModule } from './auth/auth.module';
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
           username: configService.get('DB_USER'),
-          password: configService.get('DB_PASS'),
+          password: configService.get('DB_PASSWORD'),
           database: configService.get('DB_NAME'),
           synchronize: true,
           autoLoadModels: true,
