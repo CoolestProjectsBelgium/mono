90761a1f5e65c541bfa809e7ae8da5f71f626a40
Erik Woidt <erik@woidt.be>
2026-08-01 18:33:48 +0000

    mail template redesign

--- packages/database/src/models/email_template.model.ts
+++ packages/database/src/models/email_template.model.ts
@@ -1,12 +1,27 @@
-import { Column, Table, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
+import { Column, Table, DataType } from 'sequelize-typescript';
 import { BaseEventModel } from './base_event.model.js';
+
+export enum MailTemplates {
+  registration = 'registration',
+  waiting = 'waiting',
+  welcomeOwner = 'welcomeOwner',
+  welcomeCoWorker = 'welcomeCoWorker',
+  delete = 'delete',
+  warningNoProject = 'warningNoProject',
+  deadlineApproaching = 'deadlineApproaching',
+  waitingMail = 'waitingMail',
+  activation = 'activation',
+  ask4Token = 'ask4Token',
+  emailExists = 'emailExists',
+  notifyNewProjectOwner = 'notifyNewProjectOwner',
+}
 
 @Table
 export class EmailTemplate extends BaseEventModel {
-  @Column
+  @Column({ type: DataType.ENUM(...Object.values(MailTemplates)), allowNull: false })
   declare template: string;
 
-  @Column
+  @Column({ type: DataType.ENUM('nl', 'fr', 'en'), allowNull: false })
   declare language: string;
 
   @Column
