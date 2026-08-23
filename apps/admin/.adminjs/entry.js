640d34c7aca09ec51faa5ffcc0edbbbbcfbfe0d1
Erik Woidt <erik@woidt.be>
2026-08-23 19:03:30 +0000

    fix admins bundler

--- apps/admin/.adminjs/entry.js
+++ apps/admin/.adminjs/entry.js
@@ -1,9 +1,9 @@
 AdminJS.UserComponents = {}
-import Login from '../src/components/Login'
+import Login from '../src/components/login/Login'
 AdminJS.UserComponents.Login = Login
-import MediaManagement from '../src/components/MediaManagement'
-AdminJS.UserComponents.MediaManagement = MediaManagement
-import Dashboard from '../src/components/Dashboard'
+import Dashboard from '../src/components/dashboard/Dashboard'
 AdminJS.UserComponents.Dashboard = Dashboard
+import PictureSelector from '../src/components/pictures/PictureSelector'
+AdminJS.UserComponents.PictureSelector = PictureSelector
 import PasswordEditComponent from '../../../node_modules/@adminjs/passwords/build/components/PasswordEditComponent'
 AdminJS.UserComponents.PasswordEditComponent = PasswordEditComponent
--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -4,11 +4,11 @@ import { Handler as PictureHandler } from './pictures/handler.js'
 
 const componentLoader = new ComponentLoader()
 
-componentLoader.override('Login', './components/login/Login');
+componentLoader.override('Login', './login/Login');
 
 const Components = {
   Dashboard: componentLoader.add('Dashboard', './dashboard/Dashboard'),
-  PictureSelector: componentLoader.add('PictureSelector', './picture/PictureSelector'),
+  PictureSelector: componentLoader.add('PictureSelector', './pictures/PictureSelector'),
 }
 
 const Handlers = {
